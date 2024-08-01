dcd603f14fe749114f48f75d50fa32367df55684
Danil_Krupin <133006108+Rokyl59@users.noreply.github.com>
2024-08-01 12:15:33 +0500

    Update devman_notify.py

--- devman_notify.py
+++ devman_notify.py
@@ -1,26 +1,34 @@
 import os
 import time
-import argparse
 import requests
 from dotenv import load_dotenv
 from telegram import Bot
+import logging
 
 
-if __name__ == '__main__':
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, bot, chat_id):
+        super().__init__()
+        self.bot = bot
+        self.chat_id = chat_id
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def main():
     load_dotenv()
     devman_token = os.getenv('DEVMAN_TOKEN')
     bot = Bot(token=os.getenv('TOKEN_TG'))
+    chat_id = os.getenv('CHAT_ID')
 
-    parser = argparse.ArgumentParser(
-        description='Скрипт запрашивает данные у Devman о свежих проверенных работах ученика'
-                    'И сообщает о результатах проверки ученику в созданный им телеграм бот')
-    parser.add_argument(
-        '--chat_id',
-        type=int,
-        required=True,
-        help='Идентификатор пользователя в телеграм'
-    )
-    args = parser.parse_args()
+    logger = logging.getLogger('TelegramLogger')
+    logger.setLevel(logging.ERROR)
+    handler = TelegramLogsHandler(bot, chat_id)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     headers = {
         'Authorization': f'Token {devman_token}'
@@ -32,10 +40,9 @@ if __name__ == '__main__':
     while True:
         try:
             params = {'timestamp': timestamp} if timestamp else {}
-            response = requests.get(url, headers=headers, params=params, timeout=60)
+            response = requests.get(url, headers=headers, params=params, timeout=90)
             response.raise_for_status()
             notification = response.json()
-            timestamp = notification.get('timestamp_to_request', None)
 
             if notification.get('status') == 'found':
                 for attempt in notification.get('new_attempts', []):
@@ -45,9 +52,17 @@ if __name__ == '__main__':
                     message = f'У вас проверили работу "{lesson_title}"!\n'
                     message += 'К сожалению, в работе нашлись ошибки.' if is_negative else 'Ваша работа принята без ошибок!'
                     message += f'\n\n{lesson_url}'
-                    bot.send_message(chat_id=args.chat_id, text=message)
+                    bot.send_message(chat_id=chat_id, text=message)
+
+            timestamp = notification.get('timestamp_to_request', notification.get('last_attempt_timestamp', timestamp))
 
         except requests.exceptions.ReadTimeout:
             continue
         except requests.exceptions.ConnectionError:
             time.sleep(5)
+        except Exception as e:
+            logger.error("Ошибка в основном цикле", exc_info=e)
+
+
+if __name__ == '__main__':
+    main()
